c320ddaaf882bcd244791e31777a994a2093a869
atul-mandackar <atul007atulmandavkar@gmail.com>
2024-02-18 17:47:39 +0530

    automatically add database if not present

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const mongoURI =
   "mongodb+srv://inotebook:iN0teb00k@cluster0.szrebub.mongodb.net/?retryWrites=true&w=majority/inotebook";  // in database we created a new database inotebook after / , for use of unique email address in database in url
 /* we recommend using 127.0.0.1 instead of localhost. That is because Node.js 18 and up prefer IPv6 addresses, which means, on many machines, Node.js will resolve localhost to the IPv6 address ::1 and Mongoose will be unable to connect, unless the mongodb instance is running with ipv6 enabled. */
-
+/* { autoCreate: true } is added in mongoose.connect() to create database if it is absent */
 const connectToMongo = async () =>{
     await mongoose
-      .connect(mongoURI)
+      .connect(mongoURI, { autoCreate: true })
       .then(() => console.log("Database connected!"))
       .catch((err) => console.log(err));
 }
