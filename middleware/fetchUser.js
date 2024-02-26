9dc2336f7459b839b8cfdae924dbdb016784e423
atul-mandackar <atul007atulmandavkar@gmail.com>
2024-02-26 19:18:02 +0530

    Making .env variable and adding gitignore file Firstly created .env.local file next to package.json file and in .env.local file, we add, JWT_SECRET="My name is Atul" as .env variable and we can access by using process.env.JWT_SECRET
    also next to .env.local we add gitignore file

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -1,6 +1,7 @@
-// This is a middleware function
+ // This is a middleware function
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "My name is Atul"; // this is example of jwt token secret signature which we should store in env.local or in config to keep it secrete ( here we donot saved in env.local for understanding the concept)
+//const JWT_SECRET = "My name is Atul"; // this is example of jwt token secret signature which we should store in env.local or in config to keep it secrete ( here we donot saved in env.local for understanding the concept)
+const JWT_SECRET = process.env.JWT_SECRET; // making .env variable
 
 const fetchUser = (req, res, next) => {
   // Get user from JWT token and add id to the req object
@@ -9,7 +10,7 @@ const fetchUser = (req, res, next) => {
     return res.status(401).send({ error: "Please authenticate using a valid token" }); // if token is not available then send error.
   }
 
-  try {
+  try {console.log(JWT_SECREsT);
     const data = jwt.verify(token, JWT_SECRET); // checking our JWT_SECRET (signature) whether match to the token or not with the help of verify method of jwt.
     //console.log(data); // if token match it send the user object with two keys (id and iat)
     req.user = data.id; // if the token is matched then that id from data object is send to user of req
