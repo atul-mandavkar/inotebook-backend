6319bf1e6f17a9449d7e783614e21fc5b0b35707
atul-mandackar <atul007atulmandavkar@gmail.com>
2024-02-27 18:33:00 +0530

    for correction purposee 1

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -1,13 +1,14 @@
- // This is a middleware function
+// This is a middleware function
 const jwt = require("jsonwebtoken");
-//const JWT_SECRET = "My name is Atul"; // this is example of jwt token secret signature which we should store in env.local or in config to keep it secrete ( here we donot saved in env.local for understanding the concept)
-const JWT_SECRET = process.env.JWT_SECRET || "My name is Atul"; // making .env variable
+const JWT_SECRET = "My name is Atul"; // this is example of jwt token secret signature which we should store in env.local or in config to keep it secrete ( here we donot saved in env.local for understanding the concept)
 
 const fetchUser = (req, res, next) => {
   // Get user from JWT token and add id to the req object
   const token = req.header("auth-token"); // token is taken from request header which name is auth-token
   if (!token) {
-    return res.status(401).send({ error: "Please authenticate using a valid token" }); // if token is not available then send error.
+    return res
+      .status(401)
+      .send({ error: "Please authenticate using a valid token" }); // if token is not available then send error.
   }
 
   try {
@@ -16,7 +17,9 @@ const fetchUser = (req, res, next) => {
     req.user = data.id; // if the token is matched then that id from data object is send to user of req
     next(); // next function call which is present next to call of this middleware function.
   } catch (error) {
-    return res.status(401).send({ error: "Please authenticate using a valid token" });
+    return res
+      .status(401)
+      .send({ error: "Please authenticate using a valid token" });
   }
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const fetchUser = require("../middleware/fetchUser");
 
-//const JWT_SECRET = "My name is Atul"; // this is example of jwt token secret signature which we should store in env.local or in config to keep it secrete ( here we donot saved in env.local for understanding the concept)
-const JWT_SECRET = process.env.JWT_SECRET || "My name is Atul"; // making .env variable
+const JWT_SECRET = "My name is Atul"; // this is example of jwt token secret signature which we should store in env.local or in config to keep it secrete ( here we donot saved in env.local for understanding the concept)
 
 // route 1: Create a User using : POST "/api/auth/createUser"  and also doesn't require auth. where createUser is in api/auth
 // use post method instead of get method of router because of user data is shown in get method but not in post method.
@@ -45,10 +44,10 @@ router.post(
         .catch((err) => console.log(err))
         .then(console.log("user is saved on database"));
 
-        // when user successfully saved in database then we send him a json token 
-        const token = jwt.sign({ id: user.id }, JWT_SECRET); // sign method is used to create token, first paramete is object and second parameter is signatue which is a secrete. 
-        success = true;
-        return res.send({success, token}); // we send token to who signed in instead of sending their whole information 
+      // when user successfully saved in database then we send him a json token
+      const token = jwt.sign({ id: user.id }, JWT_SECRET); // sign method is used to create token, first paramete is object and second parameter is signatue which is a secrete.
+      success = true;
+      return res.send({ success, token }); // we send token to who signed in instead of sending their whole information
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some internal error occured");
@@ -58,45 +57,53 @@ router.post(
 
 // route 2: Authenticate a User using : POST "/api/auth/login"  and also doesn't require auth. where createUser is in api/auth
 // Here only email and password is check and if not coreect email or for empty password error massage is send and also server is not triggered and when everything is correct then we check email and password available in database or not
-router.post("/login", [
-  body("email", "Enter valid email").isEmail(),
-  body("password", "Password should not empty").exists(),
-], async (req,res)=>{
-  let success = false; // deciding variable is added for future use ( it show success or not in each response)
-  const result = validationResult(req);
-  if(!result.isEmpty()){
-    return res.status(400).send({ success, errors: result.array() });    
-  }
-
-  const{email, password} = req.body; // destructuring email and password from req.body
-
-  try {
-    let user = await User.findOne({ email }); // find if user is availabe or not in database
-    if (!user) {
-      console.log("User unavailable");
-      return res.status(400).send({ success, error: "Email or password not correct" });
+router.post(
+  "/login",
+  [
+    body("email", "Enter valid email").isEmail(),
+    body("password", "Password should not empty").exists(),
+  ],
+  async (req, res) => {
+    let success = false; // deciding variable is added for future use ( it show success or not in each response)
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).send({ success, errors: result.array() });
     }
 
-    const passwordCmp = await bcrypt.compare(password, user.password); // bcript method to check password matched to the password in database for given user
-    if (!passwordCmp) {
-      console.log("User unavailable");
-      return res.status(400).send({ success, error: "Email or password not correct" });
+    const { email, password } = req.body; // destructuring email and password from req.body
+
+    try {
+      let user = await User.findOne({ email }); // find if user is availabe or not in database
+      if (!user) {
+        console.log("User unavailable");
+        return res
+          .status(400)
+          .send({ success, error: "Email or password not correct" });
+      }
+
+      const passwordCmp = await bcrypt.compare(password, user.password); // bcript method to check password matched to the password in database for given user
+      if (!passwordCmp) {
+        console.log("User unavailable");
+        return res
+          .status(400)
+          .send({ success, error: "Email or password not correct" });
+      }
+      console.log("User available");
+      success = true;
+      // when user is with correct password and email in database then we send him a json token
+      const token = jwt.sign({ id: user.id }, JWT_SECRET); // sign method is used to create token, first paramete is object and second parameter is signatue which is a secrete.
+      return res.send({ success, token }); // we send token to who signed in instead of sending their whole information
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some internal error occured");
     }
-    console.log("User available");
-    success = true;
-    // when user is with correct password and email in database then we send him a json token
-    const token = jwt.sign({ id: user.id }, JWT_SECRET); // sign method is used to create token, first paramete is object and second parameter is signatue which is a secrete.
-    return res.send({ success, token }); // we send token to who signed in instead of sending their whole information
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("some internal error occured");    
   }
-});
+);
 
 // route 3: Get login user detail using : POST "/api/auth/getUser"  and also require login
 // Here we are sending a middleware named fetchUser which gave the user id from the token from request header
 // No need to pass any checking middleware which checks whar input entered in request
-router.post("/getUser", fetchUser, async (req,res)=>{
+router.post("/getUser", fetchUser, async (req, res) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
     return res.status(400).send({ errors: result.array() });
@@ -110,6 +117,6 @@ router.post("/getUser", fetchUser, async (req,res)=>{
     console.error(error.message);
     res.status(500).send("some internal error occured");
   }
-})
+});
 
 module.exports = router;
